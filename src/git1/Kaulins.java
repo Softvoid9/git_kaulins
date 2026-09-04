42b1427852e140446bc29efe22f7fb566d627498
Softvoid <avakado@pasts.lv>
2026-09-04 09:18:25 +0300

    Programmas 1.v.

---
+++ src/git1/Kaulins.java
@@ -0,0 +1,16 @@
+package git1;
+
+import java.util.Random;
+
+public class Kaulins {
+
+	public static void main(String[] args) {
+		int skaitlis;
+		Random rand = new Random();
+		skaitlis = rand.nextInt(6)+1;
+		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		
+
+	}
+
+}
