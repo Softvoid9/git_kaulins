919e42fbd78c363d7206a250d8ccea057024a9fc
Softvoid9 <avakado@pasts.lv>
2026-09-16 13:52:47 +0300

    Atrod mazāko vērtību no visiem metiniem

--- src/git1/Kaulins.java
+++ src/git1/Kaulins.java
@@ -1,24 +1,27 @@
 package git1;
-
 import java.util.Random;
 import java.util.Scanner;
-
 public class Kaulins {
-	static void mestKaulinu(int reizes) {
-		int skaitlis;
-		Random rand = new Random();
-		for(int i=1; i<=reizes; i++) {
-			skaitlis = rand.nextInt(6)+1;
-			System.out.println("Uzkrita skaitlis: "+skaitlis);
-		}
+	static int mestKaulinu(int reizes) {
+	int skaitlis;
+	Random rand=new Random();
+	int min = 6;
+	
+	for(int i=1;i<=reizes;i++) {
+		skaitlis = rand.nextInt(6)+1;
+		System.out.println("Uzraksta skaitli: "+skaitlis);
+		if(skaitlis < min)
+			min = skaitlis;
+	}
+		return min; 
 	}
 	public static void main(String[] args) {
-		int reizes ;
+		int reizes;
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Cik reizes mest kaulinu?");
-		reizes = scan.nextInt();
-		mestKaulinu(reizes);
-
+		System.out.println("Cik reizes mest kauliņu?");
+		reizes=scan.nextInt();
+		System.out.println("Mazākais skaitlis no visiem meitieniem: "+ mestKaulinu(reizes));
+		
 		scan.close();
-	}
+}
 }
